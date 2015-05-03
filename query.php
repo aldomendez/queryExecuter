f3f0308c664149f75fe59bc905d4c1bd13440585
Aldo Mendez <aldomendez86@gmail.com>
2015-05-03 14:00:24 -0500

    con esto procesa las cosas en el server

--- query.php
+++ query.php
@@ -14,20 +14,22 @@ function index(){
     // parse_str($body, $body);
     print_r( $body );
 
-    $query = "select serial_num from carrier_site where carrier_serial_num = 'carrier'";
-    // $DB = new MxOptix();
-    // $DB->setQuery($query);
-    // oci_execute($DB->statement);
-    // $results = null;
-    // oci_fetch_all($DB->statement, $results,0,-1,OCI_FETCHSTATEMENT_BY_COLUMN);
+    // $query = "";
+    $DB = new MxOptix();
+    $DB->setQuery($body['query']);
+    echo $body['query'];
+    oci_execute($DB->statement);
+    $results = null;
+    oci_fetch_all($DB->statement, $results,0,-1,OCI_FETCHSTATEMENT_BY_ROW);
     // $json = $DB->json();
-    
+    print_r($results);
+    echo PHP_EOL;
     // if ($json == "[]") {
     //     throw new Exception("No arrojo datos la base de datos", 1);
     // } else {
     //     echo implode(',', $results['SERIAL_NUM']);
     // }
-    // $DB->close();
+    $DB->close();
 }
 
 $app->run();
