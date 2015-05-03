5a442ce6691787c56fcab034bf83cada83812853
Aldo Mendez <aldomendez86@gmail.com>
2015-05-03 18:45:09 -0500

    Ya ejecuta los querys desde un archivo

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var wait = require('gulp-wait');
 var get = require('simple-get');
 var rename = require('gulp-rename');
 var savefile = require('gulp-savefile');
+var fs = require('fs');
+var concat = require('concat-stream');
 
 var BASE = __dirname;
 
@@ -14,33 +16,57 @@ function serverPath (path) {
   return path.replace(BASE,'');
 }
 
-body = JSON.stringify({
-  query:'select sysdate from dual',
-  user:'macampos',
-  database:'mxoptix'
-});
-
-var opts = {
-  url : 'http://cymautocert/osaapp/query/query.php',
-  method: 'POST',
-  body: body,
-  headers:{
-    'user-agent':'my-cool-app'
-  }
-}
 
 copy = function copyAndReload (event) {
-  console.log('Sended to: ', event.path);
+  path = event.path.replace(BASE + '\\mxoptix\\','');
+  console.log('Processed file: ', path);
 
-  get.post(opts, function (err, res){
-    if (err) throw err
+  fs.readFile(event.path, 'utf8' , function read (err, content) {
+    
+    body = JSON.stringify({
+      query:content,
+      user:'macampos',
+      database:'mxoptix'
+    });
 
-    res.pipe(process.stdout).pipe(rename('ans.html')).pipe(savefile());  
-  })
+    var opts = {
+      url : 'http://cymautocert/osaapp/query/query.php',
+      method: 'POST',
+      body: body,
+      headers:{
+        'user-agent':'my-cool-app'
+      }
+    }
 
-  // gulp.src(event.path)
-  //      // .pipe(gulp.dest(event.path + ".csv"))
-  //      .pipe(wait(1200))
+
+
+    fs.writeFile('ask.html',JSON.stringify(opts))
+    get.post(opts, function (err, res){
+      if (err) throw err
+      string = '';
+
+      res.on('data', function(buffer){
+        // console.log('readable: ' + buffer);
+        // var part = buffer;
+        string += buffer;
+        // console.log('Streamed: \n\n' + part);
+      });
+
+      res.on('end', function () {
+        console.log('final output: \n\n' + string)
+        fs.writeFile('ans.html',string)
+      })
+
+      // console.log(res.headers)
+      // res.pipe(concat(function(data){
+      //   console.log(data);  
+      // }));  
+    })
+
+    // gulp.src(event.path)
+    //      // .pipe(gulp.dest(event.path + ".csv"))
+    //      .pipe(wait(1200))
+  });
 }
 
 
--- mxoptix/query.sql
+++ mxoptix/query.sql
@@ -1,1 +1,15 @@
-select sysdate from dual
+SELECT DISTINCT 
+a.CARRIER_SERIAL_NUM,
+a.CARRIER_SITE,
+a.SERIAL_NUM,
+a.STATUS,
+b.STATUS savedStatus,
+b.COMMENTS,
+b.component,
+b.failmode
+           
+FROM phase2.carrier_site@mxoptix a 
+,apogee.fallas_lr4@mxapps b
+WHERE a.serial_num = b.serial_num (+)
+AND a.carrier_serial_num = '155772978'       
+ORDER BY a.carrier_site
