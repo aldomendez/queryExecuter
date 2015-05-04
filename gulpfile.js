97d9b7186dd427f86f915a248ec4c1011c4ce0e6
Aldo Mendez <aldomendez86@gmail.com>
2015-05-04 17:37:13 -0500

    genera los archivos en la carpeta que genero el archivo, se ejecuta solamente cuando el cambio es en un archivo .sql

--- gulpfile.js
+++ gulpfile.js
@@ -17,63 +17,68 @@ function serverPath (path) {
 }
 
 
-copy = function copyAndReload (event) {
-  path = event.path.replace(BASE + '\\mxoptix\\','');
-  console.log('Processed file: ', path);
+copy = function copyAndReload (event, database) {
+  path = event.path.replace(BASE + '\\' + database + '\\','');
+  // Si el archivo que cambió tiene extension .sql
+  if(/sql$/i.test(path)){
 
-  fs.readFile(event.path, 'utf8' , function read (err, content) {
-    
-    body = JSON.stringify({
-      query:content,
-      user:'macampos',
-      database:'mxoptix'
-    });
+    filename = path.replace('.sql','');
+    console.log('Processed file: ', path);
 
-    var opts = {
-      url : 'http://cymautocert/osaapp/query/query.php',
-      method: 'POST',
-      body: body,
-      headers:{
-        'user-agent':'my-cool-app'
-      }
-    }
-
-
-
-    fs.writeFile('ask.html',JSON.stringify(opts))
-    get.post(opts, function (err, res){
-      if (err) throw err
-      string = '';
-
-      res.on('data', function(buffer){
-        // console.log('readable: ' + buffer);
-        // var part = buffer;
-        string += buffer;
-        // console.log('Streamed: \n\n' + part);
+    fs.readFile(event.path, 'utf8' , function read (err, content) {
+      
+      body = JSON.stringify({
+        query:content,
+        user:'macampos',
+        database:database
       });
 
-      res.on('end', function () {
-        console.log('final output: \n\n' + string)
-        fs.writeFile('ans.html',string)
+      var opts = {
+        url : 'http://cymautocert/osaapp/query/query.php',
+        method: 'POST',
+        body: body,
+        headers:{
+          'user-agent':'my-cool-app'
+        }
+      }
+
+
+
+      fs.writeFile('ask.html',JSON.stringify(opts))
+      get.post(opts, function (err, res){
+        if (err) throw err
+        string = '';
+
+        res.on('data', function(buffer){
+          // console.log('readable: ' + buffer);
+          // var part = buffer;
+          string += buffer;
+          // console.log('Streamed: \n\n' + part);
+        });
+
+        res.on('end', function () {
+          console.log('final output: \n\n' + string)
+          fs.writeFile(database + '\\' + filename + '.csv',string)
+        })
+
+        // console.log(res.headers)
+        // res.pipe(concat(function(data){
+        //   console.log(data);  
+        // }));  
       })
 
-      // console.log(res.headers)
-      // res.pipe(concat(function(data){
-      //   console.log(data);  
-      // }));  
-    })
-
-    // gulp.src(event.path)
-    //      // .pipe(gulp.dest(event.path + ".csv"))
-    //      .pipe(wait(1200))
-  });
+      // gulp.src(event.path)
+      //      // .pipe(gulp.dest(event.path + ".csv"))
+      //      .pipe(wait(1200))
+    });
+  }
 }
 
 
 gulp.task('watch', function () {
   gulp.watch(['mxoptix/**'], function (event) {
   }).on('change', function (event) {
-     copy(event);
+     copy(event, 'mxoptix');
   });
 });
 
